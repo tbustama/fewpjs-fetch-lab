effbbe7fbc5d5a2986154451adf7994184bb0f97
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-01-12 16:42:18 -0800

    commit tree 3e309fca21fafd7a8a8bd634b916c2131428fffd

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books").then(function (resp) {
+    resp.json().then((json) => renderBooks(json));
+  });
 }
-
 function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
